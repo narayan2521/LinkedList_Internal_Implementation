6bf6b3fc2b926cff1a1cd5a3edf69d25cf3a0cd0
Narayan <narayanprasad2521@gmail.com>
2023-03-11 14:57:58 +0530

    add() added in LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,20 @@
+public class LinkedList{
+    private Node first=null;
+    private int p=0;
+    public void add(Object ele)
+    {
+        if(first==null)
+        {
+            first=new Node(ele,null);
+            p++;
+            return;
+        }
+        Node curr=first;
+        while(curr.next!=null)
+        {
+            curr=curr.next;
+        }
+        curr=new Node(ele,null);
+        p++;
+    }
+}
